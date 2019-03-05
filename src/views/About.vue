11543cf08e4aefeec04cc4aa746ef2c7342e9a8a
Dan Shields <danwshields@gmail.com>
2019-03-04 23:36:39 -0700

    new about template

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,64 +3,78 @@
         <h1 class="title is-1">
             About
         </h1>
-        <p class="content">
-          <ul>
-            <li>Technical Product Development</li>
-          </ul>
-          <ul>
-            <li>Architecting and Refining Complex Systems</li>
+        <Columns>
+          <Column>
             <ul>
-              <li>Blockchain Selection and Integrations</li>
-              <li>Token Economic Implementations</li>
-              <li>Distributed Systems Design</li>
+              <li>MARKETING,
+                <ul>
+                  <li>Strategic Marketing Plan</li>
+                  <li>Whitepaper Analysis</li>
+                  <li>Pitch Decks</li>
+                  <li>Community Management</li>
+                  <li>Web Design/Graphic Design</li>
+                  <li>Bounty Campaigns</li>
+                  <li>Exchange Listings</li>
+                  <li>ICO Site Listings</li>
+                </ul>
+              </li>
+              <li>BLOCKCHAIN Integrations
+                <ul>
+                  <li>Blockchain Education&nbsp;</li>
+                  <li>Discovery Sessions</li>
+                  <li>Blockchain Selection</li>
+                  <li>System Archetecture &amp; Design</li>
+                  <li>Project Management</li>
+                  <li>Smart Contract Development</li>
+                  <li>Code Audits</li>
+                  <li>Regulatory Compliance</li>
+                </ul>
+              </li>
             </ul>
-          </ul>
-          <ul>
-            <li>White Paper Writing, Reviews, and Editing</li>
-          </ul>
-          <ul>
-            <li>Business Development</li>
+          </Column>
+          <Column>
             <ul>
-              <li>Brand Ambassador</li>
-              <li>Developer Evangelism</li>
+              <li>Technical Product Development</li>
+              <li>Architecting and Refining Complex Systems</li>
+              <ul>
+                <li>Blockchain Selection and Integrations</li>
+                <li>Token Economic Implementations</li>
+                <li>Distributed Systems Design</li>
+              </ul>
+              <li>White Paper Writing, Reviews, and Editing</li>
+              <li>Business Development</li>
+              <ul>
+                <li>Brand Ambassador</li>
+                <li>Developer Evangelism</li>
+                <li>Product Documentation Review and Generation</li>
+              </ul>
+              <li>Workshop Development &amp; facilitation</li>
+              <li>Local Meetups &amp; Webinars</li>
+              <li>Community Development</li>
+              <ul>
+                <li>Events Coordination within a large community base - Colorado Blockchain</li>
+              </ul>
+              <li>Strategy, scope and planning</li>
+              <li>Marketing and Promotion</li>
+              <li>Onsite Facilitation and Support</li>
+              <li>Media Production</li>
+              <ul>
+                <li>From basic photography to full professional productions of conferences.</li>
+              </ul>
             </ul>
-          </ul>
-          <ul>
-            <li>Workshop Development &amp; facilitation</li>
-            <li>Local Meetups &amp; Webinars</li>
-          </ul>
-          <ul>
-            <li >Community Development</li>
-            <ul>
-              <li>Events Coordination within a large community base - Colorado Blockchain</li>
-            </ul>
-          </ul>
-          <ul>
-            <li>Strategy, scope and planning</li>
-            <li>Marketing and Promotion</li>
-            <li>Onsite Facilitation and Support</li>
-          </ul>
-          <ul>
-            <li>Media Production</li>
-            <ul>
-              <li>From basic photography to full professional productions of conferences.</li>
-            </ul>
-          </ul>
-        </p>
+          </Column>
+        </Columns>
     </ch-section>
 </template>
 
 <script>
-
-import ChButton from '@/ch-components/elements/Button.vue'
-import ChButtonGroup from '@/ch-components/elements/ButtonGroup.vue'
-import ChSection from '@/ch-components/layout/Section.vue'
+import Column from '@/ch-components/columns/Column.vue'
+import Columns from '@/ch-components/columns/Columns.vue'
 
 export default {
     components: {
-        ChButton,
-        ChButtonGroup,
-        ChSection
+        Column,
+        Columns
     }
 }
 </script>
